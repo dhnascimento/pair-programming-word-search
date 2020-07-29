be6c572c0ca6460b99a44f31efe2dd9f861ca33b
Daniel Nascimento <danielhn@gmail.com>
2020-07-29 21:44:28 +0000

    Word search function after ESLint

--- wordsearch.js
+++ wordsearch.js
@@ -1,22 +1,18 @@
-const transpose = require('./transpose')
+const transpose = require('./transpose');
 // 1# find word vetically that is also the length of the array
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)){
-            return true
-        }
-        
-    }
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }   
+  };
+  let transposedLetters = transpose(letters);
+  transposedLetters = transposedLetters.map(ls => ls.join(''));
+  for (l of transposedLetters) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-    let transposedLetters = transpose(letters);
-     transposedLetters = transposedLetters.map(ls => ls.join(''))
-    for (l of transposedLetters) {
-        if (l.includes(word)) return true
-    }
-
-    return false;
-
-}
-
-module.exports = wordSearch
+module.exports = wordSearch;
